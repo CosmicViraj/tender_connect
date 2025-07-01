f33081c03b97320ae7d59abba3fd49d6e701f584
Cosmicviraj <JADHAVVIRAJ043@GMAIL.COM>
2025-07-01 15:03:38 +0530

    fix: removed duplicate supabase declaration

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,10 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import supabase from "../supabase.js";
-
 
 const supabase = createClient(
   'https://your-project.supabase.co',
-  'your-supabase-api-key' // service role key for upload
+  'your-supabase-api-key'
 );
 
 export default supabase;
